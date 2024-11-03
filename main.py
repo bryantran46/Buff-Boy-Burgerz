3d60247391500f52cc03257edad87a3013f27a52
bryantran17 <btt001@ucsd.edu>
2024-11-03 09:39:01 -0800

    Add parsing for date and regular expression to parse name and transaction amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google_apis import create_service
 from config import CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES, EMAIL_QUERY
 from email_utils import get_email_ids, get_emails, mark_emails_as_read
+import re
 
 def main():
     service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
@@ -12,7 +13,17 @@ def main():
     
     messages = get_emails(service, message_ids)
     for message in messages:
-        print(message['snippet'])
+        subject = message['snippet']
+
+        pattern = r"([A-Za-z]+\s[A-Za-z]+)\s+paid\s+you\s+\$([\d]+\.\d{2})"
+        matches = re.findall(pattern, subject)
+        # Extract and display results
+        for match in matches:
+            print(match)
+
+        for header in message['payload']['headers']:
+            if header['name'] == 'Date':
+                print(header['value'])
 
     mark_emails_as_read(service, message_ids)
 
