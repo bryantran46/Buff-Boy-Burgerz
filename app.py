97571ce3c6811835647f4d9be76ae604d026a2f2
bryantran17 <btt001@ucsd.edu>
2025-01-16 02:10:54 -0800

    Add python call to check_payment() after "I paid" button click

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from google_apis import create_service
+from config import CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES, VENMO, ZELLE 
+from email_utils import process_transaction_emails
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
+service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 @app.route('/')
 def home():
@@ -14,5 +18,38 @@ def order():
 def pay():
     return render_template('pay.html')
 
+@app.route('/check-payment', methods=['POST'])
+def check_payment():
+    # data sent by kiosk order
+    data = request.json
+    payment_type = data.get('paymentType')
+    total = data.get('total')
+    order = data.get('order')
+    print(f"Payment Type: {payment_type}")
+    print(f"Total: {total}")
+    print(f"Order: {order}")
+
+    # get transactions based on type
+    transactions = []
+    if payment_type == 'venmo':
+        transactions = process_transaction_emails(VENMO, service)
+    elif payment_type == 'zelle':
+        transactions = process_transaction_emails(ZELLE, service)
+
+    # Check if user paid
+    result = ''
+    if len(transactions) > 0:
+        sort_key, name, amount, date = transactions[0]
+        if float(amount) >= float(total):
+            result = 'Successful payment'
+        else:
+            missing_payment = float(total) - float(amount)
+            result = 'Underpaid by ' + f'{missing_payment:.2f}'
+    else:
+        result = 'No payment processed. Try again.'
+
+    print("Python script executed")
+    return jsonify({"status": "success", "message": result})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- pay.ts
+++ pay.ts
@@ -3,6 +3,7 @@ import { renderReceipt, renderTotal } from "./receipt.js";
 import { showPopup, hidePopup } from './popup.js';
 
 const popups = ["venmo", "zelle", "cash"];
+const electronicTransactions = ["venmo", "zelle"]
 
 loadData();
 renderReceipt();
@@ -22,23 +23,37 @@ popups.forEach((method) => {
     document.querySelector(`#${method}-popup .close-button`)?.addEventListener("click", () => hidePopup(`${method}-popup`));
 });
 
-document.querySelector(`#venmo-popup .paid-button`)?.addEventListener("click", async () => {
-    alert('Add stuff');
+electronicTransactions.forEach((method) => {
+    document.querySelector(`#${method}-popup .paid-button`)?.addEventListener("click", async () => {
+        const orderInfo = {
+            "paymentType": method, 
+            "total": getTotal(), 
+            "order": getOrder()
+        };
+        try {
+            const response = await fetch("/check-payment", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(orderInfo),
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const result = await response.json();
+            const responseMessageElement = document.querySelector(`#${method}-popup .response-message`);
+            if (responseMessageElement) {
+                responseMessageElement.textContent = result.message;
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            const responseMessageElement = document.querySelector(`#${method}-popup .response-message`);
+            if (responseMessageElement) {
+                responseMessageElement.textContent = "Failed to check payment";
+            }
+        }
+    })
 });
-
-
-
-/*
-
-id
-
-typescript {
-    payment method
-    total
-    cart
-}
-python {
-    name
-    time
-}
-*/
